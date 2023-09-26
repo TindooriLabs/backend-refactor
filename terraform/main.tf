17e35bd2fce97a14eb7c4d2667c6deea39c06a2a
tindoori-dragon300 <project.tindoori@gmail.com>
2023-09-26 18:11:32 -0400

    Removed additional constraint on version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.0"
-    }
-  }
   backend "s3" {
     key = "aws/terraform/terraform.tfstate"
     region = var.region

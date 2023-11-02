9e30c69b3730a0730f6a8ae6d8d2bfdc5087834f
tindoori-dragon300 <project.tindoori@gmail.com>
2023-11-02 15:29:34 -0400

    Added ingress as IP of ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,7 +147,7 @@ resource "aws_security_group" "database_security_group" {
       from_port        = 5432
       to_port          = 5432
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = [aws_eip.server_eip.public_ip]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []

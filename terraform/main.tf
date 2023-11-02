aeb2bb204e0212b414f7b50d953a746521ed9103
tindoori-dragon300 <project.tindoori@gmail.com>
2023-11-02 15:44:54 -0400

    Added public IP of backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,7 +147,7 @@ resource "aws_security_group" "database_security_group" {
       from_port        = 5432
       to_port          = 5432
       protocol         = "tcp"
-      cidr_blocks      = [aws_eip.server_eip.public_ip] / 32
+      cidr_blocks      = ["${aws_eip.server_eip.public_ip}/32"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []

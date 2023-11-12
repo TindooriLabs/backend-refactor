cab20db0440f2a03f78d23c25a563c5939798898
tindoori-dragon300 <project.tindoori@gmail.com>
2023-11-12 03:44:00 -0500

    Added rules for fixing error

--- terraform/main.tf
+++ terraform/main.tf
@@ -223,6 +223,17 @@ resource "aws_security_group" "main_security_group" {
       prefix_list_ids  = []
       security_groups  = []
       self             = false
+    },
+     {
+      description      = "POSTGRES"
+      from_port        = 5432
+      to_port          = 5432
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = [aws_security_group.database_security_group.id]
+      self             = false
     }
   ]
 

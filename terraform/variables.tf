d5769b0f110d6d69d8c0fdb56423b3ab896acb24
tindoori-dragon300 <project.tindoori@gmail.com>
2023-11-12 02:49:19 -0500

    Revert "Added changes to fix issues"
    This reverts commit 6ecb23e3a3bbc950ba690fc8d36894f828b213c8.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,11 +15,11 @@ variable "aws_public_availability_zone" {
 }
 variable "aws_private_availability_zone_1" {
   type    = string
-  default = "us-east-1a"
+  default = "us-east-1b"
 }
 variable "aws_private_availability_zone_2" {
   type    = string
-  default = "us-east-1b"
+  default = "us-east-1c"
 }
 variable "key_name" {
 

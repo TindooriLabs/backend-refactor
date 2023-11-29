e4c5a4a1c88bb47f8dcc3a1408315cd51734116e
tindoori-dragon300 <project.tindoori@gmail.com>
2023-11-29 17:37:34 -0500

    Updated code to send token in header

--- socket/middleware.js
+++ socket/middleware.js
@@ -7,7 +7,7 @@ export function socketLog(socket, next) {
 
 //Note: this is manual since I had trouble getting passport to integrate https://socket.io/docs/v4/middlewares/#compatibility-with-express-middleware
 export function socketJwtAuth(socket, next) {
-  const token = socket.handshake.auth.token;
+  const token = socket.handshake.headers.authorization;
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
     if (!user.userId) {

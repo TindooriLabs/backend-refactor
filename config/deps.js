a8a425a0207c93a69dd03571078f00034c25f736
tindoori-dragon300 <project.tindoori@gmail.com>
2023-12-18 17:16:52 -0500

    Updated email override to false

--- config/deps.js
+++ config/deps.js
@@ -11,7 +11,7 @@ const buildDeps = async (app) => {
   const tempToggles = {
     "sms-otp-override": false,
     "google-translate-override": false,
-    "email-otp-override": true,
+    "email-otp-override": false,
     "notification-override": true,
   };
   app.set("featureToggles", tempToggles);
